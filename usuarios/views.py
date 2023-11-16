794e6c1c2b28e2efbe43a2e3a247b41a9f1a7825
carloscfas <Carloasccasd15574@outlook.com>
2023-11-16 13:30:03 -0300

    Atualizando com Forms

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from usuarios.forms import LoginForms
+from usuarios.forms import LoginForms, CadastroForms
 
 # Create your views here.
 
@@ -8,4 +8,5 @@ def login(request):
     return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroForms()
+    return render(request, 'usuarios/cadastro.html', {'form': form})
